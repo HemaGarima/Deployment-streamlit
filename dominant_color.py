ad8270f3c0b34666b42749111c013178389dd86d
Hema <hemlatasharma23052003@gmail.com>
2024-06-29 11:02:02 +0530

    Making web app for dominant color extraction of any image

---
+++ dominant_color.py
@@ -0,0 +1,36 @@
+import streamlit as st
+from sklearn.cluster import KMeans
+import numpy as np
+import matplotlib.pyplot as plt
+from PIL import Image
+
+st.title("Dominant Color Extraction")
+
+st.subheader("Input Image")
+img = st.file_uploader("Choose an image")
+
+if img is not None:
+    st.header("Original Image")
+    st.image(img)
+
+    # KMeans Code
+    print(type(img))
+
+    img = plt.imread(img)
+    print(type(img))
+    print(img.shape)
+    n = img.shape[0]*img.shape[1]
+    all_pixels = img.reshape((n,3))
+    model = KMeans(n_clusters=3)
+    model.fit(all_pixels)
+    centers = model.cluster_centers_.astype('uint8')
+    new_img = np.zeros((n,3),dtype = 'uint8')
+
+    for i in range(n):
+        group_idx = model.labels_[i]
+        new_img[i] = centers[group_idx]
+
+    new_img = new_img.reshape(*img.shape)
+
+    st.header("Modified Image")
+    st.image(new_img)
